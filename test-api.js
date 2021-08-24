2b5f0b882e67178de9f4873d1c8f50d7cae34801
Oro Kurnia <orokurnia2@outlook.com>
2021-08-24 21:42:53 +0700

    fixing

--- test-api.js
+++ test-api.js
@@ -1,33 +1,18 @@
-const request = require('supertest');
-const should = require('should');
-const express = require('express');
-const cookieParser = require('cookie-parser');
+const request = require("supertest");
+const ApiUrl = "https://jsonplaceholder.typicode.com";
 
-describe('request.agent(app)', function() {
-  const app = express();
-  app.use(cookieParser());
 
-  app.get('/', function(req, res) {
-    res.cookie('cookie', 'hey');
-    res.send();
-  });
-
-  app.get('/return', function(req, res) {
-    if (req.cookies.cookie) res.send(req.cookies.cookie);
-    else res.send(':(')
-  });
-
-  const agent = request.agent(app);
-
-  it('should save cookies', function(done) {
-    agent
-    .get('/')
-    .expect('set-cookie', 'cookie=hey; Path=/', dongite);
-  });
-
-  it('should send cookies', function(done) {
-    agent
-    .get('/return')
-    .expect('hey', done);
+describe("GET /posts", () => {
+  it("should return 200 and an array of posts", async () => {
+    return request(ApiUrl)
+      .get("/posts")
+      .expect(200)
+      .set("Accept", "application/json")
+      .expect("Content-Type", /json/)
+      .then((response) => {
+        const { body } = response;
+        expect(body).toEqual(expect.any(Array));
+        expect(body.length).toEqual(expect.any(Number));
+      });
   });
 });
